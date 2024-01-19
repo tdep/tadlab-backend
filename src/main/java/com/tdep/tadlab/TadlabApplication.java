7a1ab28909d491cd906d29ff80d4156b3954be92
Trevor DePew <tdepew@spotify.com>
2024-01-19 11:57:03 -0500

    make it do things

---
+++ src/main/java/com/tdep/tadlab/TadlabApplication.java
@@ -0,0 +1,12 @@
+package com.tdep.tadlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadlabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TadlabApplication.class, args);
+  }
+}
