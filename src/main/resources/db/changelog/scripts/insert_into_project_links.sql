181a08f998009ca9432a5091d0c5ab8c1ae2a0b4
tdep <t.depewmusic@gmail.com>
2024-03-13 08:42:03 -0400

    adds project and tool tables

--- src/main/resources/db/changelog/scripts/insert_into_project_links.sql
+++
@@ -1,6 +0,0 @@
-insert into project_links (project_id, name, url, link_type)
-VALUES
-    (1, 'project-link-1', 'project-link-url-1', 'demo'),
-    (2, 'project-link-1', 'project-link-url-2', 'github'),
-    (3, 'project-link-1', 'project-link-url-3', 'demo'),
-    (4, 'project-link-1', 'project-link-url-4', 'github');
--- src/main/resources/db/changelog/scripts/insert_into_tools.sql
+++
@@ -1,6 +0,0 @@
-insert into tools (name, image_url, project_id)
-VALUES
-    ('test-tool-1', 'tool-image-url-1', 1),
-    ('test-tool-2', 'tool-image-url-2', 2),
-    ('test-tool-3', 'tool-image-url-3', 3),
-    ('test-tool-4', 'tool-image-url-4', 4);
