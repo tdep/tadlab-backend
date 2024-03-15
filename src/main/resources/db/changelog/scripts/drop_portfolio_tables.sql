c1040defb6f35690eada7f43ec5ef8825317731f
tdep <t.depewmusic@gmail.com>
2024-03-15 13:17:24 -0400

    updates portfolio schema

--- src/main/resources/db/changelog/scripts/drop_portfolio_tables.sql
+++ src/main/resources/db/changelog/scripts/drop_portfolio_tables.sql
@@ -2,5 +2,6 @@
 drop table if exists portfolio_entries cascade;
 drop table if exists projects cascade;
 drop table if exists tools, urls, portfolio_entry_urls, project_tools;
+drop type if exists link_types, url_types, entry_types;
 
 
