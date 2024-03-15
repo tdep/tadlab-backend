eaf22965fcb5b8637fcd41ce07b02dedf1f6800b
tdep <t.depewmusic@gmail.com>
2024-03-15 12:32:12 -0400

    triggering redeploy

--- src/main/resources/db/changelog/scripts/drop_portfolio_tables.sql
+++ src/main/resources/db/changelog/scripts/drop_portfolio_tables.sql
@@ -1,5 +1,6 @@
 --changelog tdep:1
-drop table if exists portfolio_entries, projects cascade;
+drop table if exists portfolio_entries cascade;
+drop table if exists projects cascade;
 drop table if exists tools, urls, portfolio_entry_urls, project_tools;
 
 
