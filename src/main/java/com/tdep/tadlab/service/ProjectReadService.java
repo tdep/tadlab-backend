8d42144b68b831db0444c9d7c5e559e84a376c04
tdep <t.depewmusic@gmail.com>
2024-03-21 12:16:10 -0400

    sets up new read/write service methods

--- src/main/java/com/tdep/tadlab/service/ProjectReadService.java
+++ src/main/java/com/tdep/tadlab/service/ProjectReadService.java
@@ -9,11 +9,24 @@ import java.util.List;
 
 public interface ProjectReadService {
 
+//    Project
     ResponseEntity<List<Project>> getAllProjects();
-    ResponseEntity<List<ProjectDetail>> getAllDetails();
-    ResponseEntity<List<Link>> getAllLinks();
-
     ResponseEntity<Project> getProjectById(int id);
+    ResponseEntity<Project> findProjectByName(String name);
+    ResponseEntity<Project> findProjectByLink(String linkName);
+    ResponseEntity<Project> findProjectByDate(String date);
+
+//    Project Details
+    ResponseEntity<List<ProjectDetail>> getAllDetails();
     ResponseEntity<ProjectDetail> getDetailById(int id);
+    ResponseEntity<ProjectDetail> findDetailByProjectName(String projectName);
+    ResponseEntity<ProjectDetail> findDetailByProjectId(int projectId);
+
+//    Link
+    ResponseEntity<List<Link>> getAllLinks();
     ResponseEntity<Link> getLinkById(int id);
+    ResponseEntity<Link> findLinkByType(String type);
+    ResponseEntity<Link> findLinkByProjectName(String projectName);
+    ResponseEntity<Link> findLinkByProjectId(int projectId);
+
 }
--- src/main/java/com/tdep/tadlab/service/ProjectWriteService.java
+++ src/main/java/com/tdep/tadlab/service/ProjectWriteService.java
@@ -1,4 +1,36 @@
 package com.tdep.tadlab.service;
 
+import com.tdep.tadlab.entity.projectDb.Link;
+import com.tdep.tadlab.entity.projectDb.Project;
+import com.tdep.tadlab.entity.projectDb.ProjectDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface ProjectWriteService {
+
+//    Project
+    ResponseEntity<Project> createNewProject(Project project);
+    ResponseEntity<Project> updateExistingProject(int id, Project project);
+    ResponseEntity<Project> addDetailToProject(int id, ProjectDetail detail);
+    ResponseEntity<Project> addLinkToProject(int id, Link link);
+    ResponseEntity<Project> addMultipleLinksToProject(int id, List<Link> links);
+    ResponseEntity<Project> removeDetailFromProject(int id, ProjectDetail detail);
+    ResponseEntity<Project> removeLinkFromProject(int id, Link link);
+    ResponseEntity<Project> removeAllLinksFromProject(int id);
+    ResponseEntity<HttpStatus> deleteProjectById(int id);
+    ResponseEntity<HttpStatus> deleteAllProjects();
+
+//    Project Detail
+    ResponseEntity<ProjectDetail> createNewProjectDetail(int projectId, ProjectDetail detail);
+    ResponseEntity<ProjectDetail> updateExistingProjectDetail(int projectId,ProjectDetail detail);
+    ResponseEntity<HttpStatus> deleteProjectDetailByProjectId(int projectId);
+    ResponseEntity<HttpStatus> deleteProjectDetailByProjectName(String projectName);
+
+//    Link
+    ResponseEntity<Link> createNewLink(Link link);
+    ResponseEntity<Link> updateExistingLink(int id, Link link);
+    ResponseEntity<Link> updateExistingProjectLink(int projectId, Link link);
+    ResponseEntity<HttpStatus> deleteLink(int id);
 }
