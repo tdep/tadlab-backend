23f6cf241a0086b0661853164aa00bdb676cd1b5
tdep <t.depewmusic@gmail.com>
2024-03-28 14:32:37 -0400

    changes cross origin

--- src/main/java/com/tdep/tadlab/controller/ProjectController.java
+++ src/main/java/com/tdep/tadlab/controller/ProjectController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "PostmanRuntime/7.37.0")
+@CrossOrigin(origins = "http:localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class ProjectController {
