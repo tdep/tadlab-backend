1ff34a1bd74d6adf90807a7a3b25ced783bcec47
tdep <t.depewmusic@gmail.com>
2024-03-28 14:54:34 -0400

    fixed typo

--- src/main/java/com/tdep/tadlab/controller/ProjectController.java
+++ src/main/java/com/tdep/tadlab/controller/ProjectController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http:localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class ProjectController {
