72a52794ff218cbad82af2e0e57bf8fd77e73dcf
tdep <t.depewmusic@gmail.com>
2024-03-28 16:41:28 -0400

    builds job read/write services, small changes to project services

--- src/main/java/com/tdep/tadlab/service/JobReadServiceImpl.java
+++ src/main/java/com/tdep/tadlab/service/JobReadServiceImpl.java
@@ -5,10 +5,13 @@ import com.tdep.tadlab.repository.JobRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobReadServiceImpl implements JobReadService {
@@ -19,11 +22,29 @@ public class JobReadServiceImpl implements JobReadService {
     Logger logger = LoggerFactory.getLogger(ProjectWriteServiceImpl.class);
 
     public ResponseEntity<List<Job>> findAllJobs() {
-        return null;
+        try {
+            List<Job> jobs = new ArrayList<>(
+                    jobRepository.findAll());
+            if (jobs.isEmpty()) {
+                logger.info("No Jobs found.");
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(jobs, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(String.format("Could not find Jobs because of exception: %s", e.getMessage()));
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<Job> findJobById(int id) {
-        return null;
+        Optional<Job> jobData = jobRepository.findById(id);
+
+        return jobData.map(
+                job -> new ResponseEntity<>(
+                        job, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(
+                        HttpStatus.NOT_FOUND));
     }
 
     public ResponseEntity<Job> findJobByName(String name) {
--- src/main/java/com/tdep/tadlab/service/JobWriteServiceImpl.java
+++ src/main/java/com/tdep/tadlab/service/JobWriteServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class JobWriteServiceImpl implements JobWriteService {
 
@@ -19,18 +21,69 @@ public class JobWriteServiceImpl implements JobWriteService {
     Logger logger = LoggerFactory.getLogger(JobWriteServiceImpl.class);
 
     public ResponseEntity<Job> createNewJob(Job job) {
-        return null;
+        try {
+
+            Job _job = jobRepository
+                    .save(new Job(
+                            job.getEntryName(),
+                            job.getEntryType(),
+                            job.getCreatedBy(),
+                            job.getCreatedAt(),
+                            job.getName(),
+                            job.getStartDate(),
+                            job.getEndDate(),
+                            job.getJobDetail()
+                    ));
+
+            logger.info(String.format("Job: %s created successfully!", _job.getId()));
+            return new ResponseEntity<>(_job, HttpStatus.CREATED);
+        } catch (Exception e) {
+            logger.error(String.format("Couldn't create Job with error: %s", e.getMessage()));
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<Job> updateExistingJob(int id, Job job) {
-        return null;
+        Optional<Job> jobData = jobRepository.findById(id);
+
+        if(jobData.isPresent()) {
+            Job _job = jobData.get();
+            _job.setEntryName(job.getEntryName());
+            _job.setEntryType(job.getEntryType());
+            _job.setCreatedBy(job.getCreatedBy());
+            _job.setUpdatedBy(job.getUpdatedBy());
+            _job.setUpdatedAt(job.getUpdatedAt());
+            _job.setName(job.getName());
+            _job.setStartDate(job.getStartDate());
+            _job.setEndDate(job.getEndDate());
+            _job.setJobDetail(job.getJobDetail());
+            logger.info("Job updated successfully!");
+            return new ResponseEntity<>(jobRepository.save(_job), HttpStatus.OK);
+        } else {
+            logger.error("Job does not exist");
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
     }
 
     public ResponseEntity<HttpStatus> deleteJobById(int id) {
-        return null;
+        try {
+            jobRepository.deleteById(id);
+            logger.info("Job deleted successfully!");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            logger.error(String.format("Unable to delete this Job because: %s", e.getMessage()));
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<HttpStatus> deleteAllJobs() {
-        return null;
+        try {
+            jobRepository.deleteAll();
+            logger.info("All Jobs deleted successfully!");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            logger.error(String.format("Unable to delete all Jobs because: %s", e.getMessage()));
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
