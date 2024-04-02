3e9eeb0d09af9bc8dad5f9bfe24cf7ca09a10636
tdep <t.depewmusic@gmail.com>
2024-04-02 14:15:53 -0400

    updates cross origins list

--- src/main/java/com/tdep/tadlab/TadlabApplication.java
+++ src/main/java/com/tdep/tadlab/TadlabApplication.java
@@ -28,7 +28,7 @@ public class TadlabApplication {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1")
-            .allowedOrigins("http://localhost:3000");
+            .allowedOrigins("http://localhost:3000", "PostmanRuntime/7.37.0");
       }
     };
   }
